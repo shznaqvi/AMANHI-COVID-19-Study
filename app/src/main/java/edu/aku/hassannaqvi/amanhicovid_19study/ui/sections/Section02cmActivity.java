9be4f02d23c23bc37d8ee9b1a4557ce357ddd2e3
SAJID LATIF <indus1530@gmail.com>
2021-01-19 15:04:13 +0500

    someLayoutChanges

--- app/src/main/java/edu/aku/hassannaqvi/amanhicovid_19study/ui/sections/Section02cmActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/amanhicovid_19study/ui/sections/Section02cmActivity.java
@@ -1,10 +1,17 @@
 package edu.aku.hassannaqvi.amanhicovid_19study.ui.sections;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
-import android.os.Bundle;
-import android.view.View;
+import com.validatorcrawler.aliazaz.Clear;
+
+import org.jetbrains.annotations.NotNull;
 
 import edu.aku.hassannaqvi.amanhicovid_19study.R;
 import edu.aku.hassannaqvi.amanhicovid_19study.databinding.ActivitySection02cmBinding;
@@ -18,7 +25,25 @@ public class Section02cmActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         bi = DataBindingUtil.setContentView(this, R.layout.activity_section_02cm);
         bi.setCallback(this);
+        setupSkips();
     }
+
+
+    private void setupSkips() {
+        rgListener(bi.cm0201, bi.cm020111, bi.cvcm0202);
+    }
+
+
+    public void rgListener(@NotNull RadioGroup rg, RadioButton rb, ViewGroup vg) {
+        rg.setOnCheckedChangeListener((radioGroup, i) -> {
+            Clear.clearAllFields(vg);
+            vg.setVisibility(View.VISIBLE);
+            if (i == rb.getId()) {
+                vg.setVisibility(View.GONE);
+            }
+        });
+    }
+
 
     public void BtnEnd() {
 
@@ -27,4 +52,6 @@ public class Section02cmActivity extends AppCompatActivity {
     public void BtnContinue() {
 
     }
+
+
 }
